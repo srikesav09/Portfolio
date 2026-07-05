19ca2220cca6cd0bf6b1a20b1a8760c4cab8dea8
srikesav09 <srikesav77@gmail.com>
2026-07-05 20:35:20 +0530

    Added Card and SectionTitle component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 
-import Navbar from './components/layout/Navbar';
-import Hero from "./components/sections/Hero";
+import Button from './components/ui/Button';
+import Card from "./components/ui/Card";
+import SectionTitle from './components/ui/SectionTitle';
+import Container from "./components/ui/Container";
+
+
 
 function App() {
   return (
-    <div className="bg-scale-900 text-white">
-      <Navbar />
-      <Hero />
+
+    <div className="min-h-screen bg-slate-950 py-20">
+      <Container>
+        <SectionTitle 
+        subtitle="Testing"
+        title="Design System" />
+
+        <Card className="w-full max-w-lg space-y-6">
+          <p>
+            This our resuable card.
+          </p>
+
+          <div className="flex flex-col sm:flex-row gap-4">
+            <Button>Primary</Button>
+            <Button variant="outline">
+              outline
+            </Button>
+            <Button variant="secondary">
+              secondary
+            </Button>
+          </div>
+        </Card>
+      </Container>
     </div>
   );
 }
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -5,25 +5,25 @@ export default function Button({
   href
 }) {
 
-  const base = 
-  "px-6 py-3 rounded-2xl font-medium transition-all duration-300";
+  const base =
+  "inline-flex items-center justify-center px-6 py-3 rounded-2xl font-medium transition-all duration-300 cursor-pointer hover:scale-105";
 
   const styles = {
     primary:
-      "bg-blue-600 hover:bg-blue-500 text-white",
+      "bg-blue-500 text-white hover:bg-blue-600",
     
       secondary:
-        "border border-slate-600 hover: border-blue-500 hover: text-blue-400",
+        "border border-slate-400 hover:border-blue-500 hover:text-blue-500",
 
       outline:
-        "border border-blue-500 text-blue-400 hover:bg-blue-500 hover:text-white"
+        "border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white"
   };
 
   if(href){
     return (
       <a
         href={href}
-        className={`$(base) $(styles[variant])`}>
+        className={`${base} ${styles[variant]}`}>
           {children}
       </a>
     )
@@ -32,9 +32,10 @@ export default function Button({
   return (
     <button
       onClick={onClick}
-      className={`$(base) $(styles[variant])`}
+      className={`${base} ${styles[variant]}`}
       >
         {children}
       </button>
   );
 }
+
